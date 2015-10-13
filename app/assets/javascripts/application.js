42d46e98f3c06ea27396d5ed362a41dc43b4a8d7
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-13 15:43:06 -0700

    Working on bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,10 @@
 // about supported directives.
 //
 
-//= require jquery.js
-//= require bootstrap.js
-//= require bootstrap.min.js
-//= require contact_me.js
-//= require jqBootstrapValidation.js
+//= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require contact_me
+//= require jqBootstrapValidation
 //= require_tree .
