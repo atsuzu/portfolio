b36ee5d2ebff22687ca46fd4b50ba83ce55e5d36
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-22 16:52:59 -0700

    Still trying to fix icons

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Portfolio
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += ["fontawesome-webfont.ttf",
                              "fontawesome-webfont.eot",
                              "fontawesome-webfont.svg",
