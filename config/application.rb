3deab1e214e5663aa4a3cc05219e35f16a266dbc
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-22 17:32:39 -0700

    Still trying to fix icons

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,17 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 config.assets.precompile += ["fontawesome-webfont.ttf",
                              "fontawesome-webfont.eot",
                              "fontawesome-webfont.svg",
                              "fontawesome-webfont.woff",
                              "fontawesome-webfont.woff2"]
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
