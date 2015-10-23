1f7aee7365d82be6359bd7259aef358112a82b65
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-22 18:51:51 -0700

    Attempting to add font awesome again

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Portfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += %w( .svg .eot .woff .ttf)
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += ["fontawesome-webfont.ttf",
                              "fontawesome-webfont.eot",
                              "fontawesome-webfont.svg",
