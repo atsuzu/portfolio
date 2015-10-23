4ce42d511a468956f0b3381ad8769c1d88bcd2e2
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-22 17:27:14 -0700

    Still trying to fix icons

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.precompile += ["fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff",
+                             "fontawesome-webfont.woff2"]
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,10 +29,6 @@ module Portfolio
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += ["fontawesome-webfont.ttf",
-                             "fontawesome-webfont.eot",
-                             "fontawesome-webfont.svg",
-                             "fontawesome-webfont.woff",
-                             "fontawesome-webfont.woff2"]
+
   end
 end
