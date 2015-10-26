982a367697023d4b11ed6cf5766f2afda039d6ed
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-26 16:32:44 -0700

    Added contents to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.create(title: "Distance and Volume Meter", image: "https://dl.dropboxusercontent.com/u/60265821/Images/PortfolioImages/DistanceandVolumeMeter.png", description: "A project created in embedded systems class.", skills: "C, Atmel Studios, Embedded Systems", date: "April 2014")
+
+Project.create(title: "Kareshi!", image: "https://dl.dropboxusercontent.com/u/60265821/Images/PortfolioImages/eCommerce.png", description: "An eCommerce website that sells anime products.", skills: "HTML, CSS, Javascript", date: "April 2015")
+
+Project.create(title: "FabFlix", image: "https://dl.dropboxusercontent.com/u/60265821/Images/PortfolioImages/FabFlix.png", description: "A movie rental website that competes with Netflix", skills: "HTML, CSS, Javascript, JSP, MySQL", date: "May 2015")
