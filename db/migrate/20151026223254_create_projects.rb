d067c75c9851a165a219a95339d41879e9350adc
Atsuhiro Sean Suzuki <suzatsuhiro@gmail.com>
2015-10-26 15:38:22 -0700

    Migrated database table

--- db/migrate/20151026223254_create_projects.rb
+++ db/migrate/20151026223254_create_projects.rb
@@ -2,6 +2,11 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
 
+		t.string :title
+		t.string :image
+		t.string :description
+		t.string :skills
+		t.string :date
       t.timestamps null: false
     end
   end
